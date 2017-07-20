f903d1d6724970dca6797cbb3e42be9e2222bac8
jordenjolley <bellaachu23@aol.com>
2017-07-20 10:06:41 -0400

    Added text adventure folder

---
+++ Python/text_adventure.py
@@ -0,0 +1,32 @@
+start = '''
+When you wake up do you ever consider how harmful your dicisions are to the enviroment when it comes to simple
+daily actions? For isnstance throwing out trash, for a few the answer will be yes but for the majority the answer will be no.
+ Recycling is of great imporance as waste has numerous negative effects on the enviroment. Imagine you are walking home from school today
+ and you see a plastic item on the ground would you put it in the "plastic" bin or in the "trash" bin'''
+
+
+print(start)
+
+
+print("Type 'plastic' to put plastic in recyling bin for plastic  or 'trash' to put trash in recycling bin for trash.")
+user_input = input()
+if user_input == 'plastic':
+    user_input = input('''You have decided to put a plastic item in the Recycling bin for plastic, now you have helped to reduce the amount
+     of pollution caused by waste. While walking you see another peice of waste, a glass bottle. Do you want to put it in the
+      glass recycling bin or the trash bin?''')
+           # finished the story by writing what happens
+    if user_input == 'glass':
+        user_input = input('good job you have dont it again')
+    elif user_input == 'trash'
+        user_input = input('''ooops looks like you made a mistake.Lets test your recycling skills once moreself.you
+        see a crumpled peice of paper on the ground would you put it in the recycling bin for paper or the recycling bin for trash''')
+        if user_input == 'paper':
+            user_input = input('''great job you are now ready to excercise your recycling skills in the
+            real world)ss
+        elif user_input == 'trash'
+
+
+elif user_input == "trash":
+    print("You have choosen to put a plastic item in the trash bin, now you have contributed to the pollution caused by waste...")
+     #
+    finished the story writing what happens
